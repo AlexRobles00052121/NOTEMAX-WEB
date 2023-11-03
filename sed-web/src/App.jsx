6228d4d53ef1bf79baf50b8c090e3bd3e4c58d4e
Fabio-Alfredo <00210121@uca.edu.sv>
2023-11-03 17:56:04 -0600

    add: Not Found

--- sed-web/src/App.jsx
+++ sed-web/src/App.jsx
@@ -3,8 +3,10 @@ import InApp from './page/InApp/InApp';
 import Login from './page/Login/Login';
 import Registrer from './page/Register/Register';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import NotFound from './components/NotFound/NotFound';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useLocalStorage } from 'react-use';
+
 
 
 function App() {
@@ -15,11 +17,12 @@ function App() {
 
     <Router>
       <Routes>
-        <Route index element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route path='/registrer' element={<Registrer/>} />
         <Route element={<PrivateRoute canActivate={user}/>}>
           <Route path='/principal' element={<InApp />} />
         </Route>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </Router>
 
---
+++ sed-web/src/components/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css'; 
+
+const NotFound = () => {
+  return (
+    <div className="container">
+      <p className="error-text">Página no encontrada</p>
+    </div>
+  );
+};
+
+export default NotFound;
+
