dc93052f905e2e437fb05f9f2a08b980ff0345ae
AlexRobles00052121 <00052121@uca.edu.sv>
2023-11-03 09:07:53 -0600

    📝add new view

--- sed-web/src/App.jsx
+++ sed-web/src/App.jsx
@@ -1,21 +1,16 @@
 import './App.css'
-import Header from './components/Header/Header';
-import NewNoteForm from './components/Notes/NewNote/NewNoteForm';
+import InApp from './page/InApp/InApp';
 import Login from './page/Login/Login';
-import Registrer from './page/Register/Register';
+
 
 function App() {
 
   return (
     <>
 
-      {/*<Header />
-    <NewNoteForm />
-    <Registrer/>
-     >*/
-      }
       <Login/>
 
+      <InApp />
 
     </>
   )
---
+++ sed-web/src/page/InApp/InApp.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header/Header';
+import NewNoteForm from '../../components/Notes/NewNote/NewNoteForm'
+
+const InApp = () => {
+    return (
+        <>
+        <Header />
+        <NewNoteForm />
+        </>
+    );
+}
+
+export default InApp;
